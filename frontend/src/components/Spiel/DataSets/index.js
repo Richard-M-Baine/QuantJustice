0ef7fb8c20716cc694a495c71aed808b9cd3f7b2
Richard Baine <efdeliveryservice@gmail.com>
2026-07-01 20:13:44 -0400

    bloody error

--- frontend/src/components/Spiel/DataSets/index.js
+++ frontend/src/components/Spiel/DataSets/index.js
@@ -2,48 +2,7 @@ import React from 'react';
 import './dataSets.css';
 import image from '../../../assets/download.jpg'
 
-const files = [
-    {
-        file: "countyCrimes.csv",
-        description: "Summarized county averages for each crime",
-        size: '923KB'
-    },
-    {
-        file: "judgeCrimes.csv",
-        description: "Summarized averages for crimes by each individual judge",
-        size: '3.57 MB'
-    },
-    {
-        file: "nationTwo.csv",
-        description: "Police misconduct data set #2",
-        size: '4.07 MB'
-    },
-    {
-        file: "nationWideOne.csv",
-        description: "Police misconduct data set #1",
-        size: '141 KB'
-    },
-    {
-        file: "offense_counts.csv",
-        description: "Year-on-year averages for each crime",
-        size: '713 KB'
-    },
-    {
-        file: "textcsv.csv",
-        description: "Police misconduct for New Jersey",
-        size: '93 KB'
-    },
-    {
-        file: "totalCrime.csv",
-        description: "Summarized totals for each crime",
-        size: '93 KB'
-    },
-    {file: "newOne.csv",
-      description: "raw data.  Names hashed",
-      size: '24.71 MB'
-    }
 
-];
 
 function DataSetsComponent() {
   return (
